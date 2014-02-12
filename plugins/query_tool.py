387bccd04644a3f01de8b5d2822045ee00c1ff07
fffonion <fffonion@gmail.com>
2014-02-13 00:27:06 +0800

    query_tool增加国服&台服排名查询(qr)

--- plugins/query_tool.py
+++ plugins/query_tool.py
@@ -7,9 +7,9 @@ from cross_platform import *
 # start meta
 __plugin_name__ = 'query infomation of player'
 __author = 'fffonion'
-__version__ = 0.2
+__version__ = 0.3
 hooks = {}
-extra_cmd = {'q_item':'query_item', 'qi':'query_item', 'q_holo':'query_holo', 'qh':'query_holo', 'qgc':'query_guild_contribution'}
+extra_cmd = {'q_item':'query_item', 'qi':'query_item', 'q_holo':'query_holo', 'qh':'query_holo', 'qgc':'query_guild_contribution','q_rank':'query_rank','qr':'query_rank'}
 # end meta
 
 # query item count
@@ -68,4 +68,70 @@ def query_guild_contribution(plugin_vals):
             if c:
                 cnt += int(c[0])
         print(du8('公会贡献: %d/ %s'%(cnt,total or '?')))
+    return do
+
+def query_rank(plugin_vals):
+    def do(*args):
+        logger = plugin_vals['logger']
+        loc = plugin_vals['loc']
+        if loc[:2] not in ['cn','tw']:
+            logger.error('排位查询不支持日服和韩服')
+            return
+        if loc == 'tw':
+            import _query_rank_tw_lib as _lib
+            import re
+            import urllib
+            if PYTHON3:
+                import urllib.request as urllib2
+                opener = urllib2.build_opener(urllib2.ProxyHandler(urllib.request.getproxies()))
+            else:
+                import urllib2
+                opener = urllib2.build_opener(urllib2.ProxyHandler(urllib.getproxies()))
+            _header = _lib.broswer_headers
+            _header['cookie'] = plugin_vals['cookie']
+            _header['User-Agent'] = plugin_vals['poster'].header['User-Agent']
+            _guild_mode = raw_inputd('查询个人排名(s)还是公会排名(g)> ') == 'g'
+            _goto = raw_inputd('输入要查询的排名开始数，按回车显示自己所在区域> ')
+            def show_it(content):
+                strl = '\n%s\n%s\n' %(_lib.query_title(content),'-'*20)
+                for (k, v) in (_guild_mode and _lib.query_guild or _lib.query_self):
+                    try:
+                        strl += '%s %s\n' % (k, v(content))
+                    except IndexError:
+                        pass
+                logger.info(strl)
+            if _goto:
+                if not _goto.isdigit() or \
+                    (int(_goto)>20000 and not _guild_mode) or (int(_goto)>2000 and _guild_mode) or \
+                    int(_goto)<=0:
+                    logger.error('请输入%d以内0以上的数字' % (2000 if _guild_mode else 20000))
+                    return
+                x = opener.open(urllib2.Request(
+                        (_guild_mode and _lib.tw_query_guild_goto or _lib.tw_query_self_goto) % _goto,
+                         headers = _header)
+                    ).read()
+                show_it(x)
+            else:
+                if _lib.now >= _lib.tw_query_lifetime:
+                    logger.error('查询库已过期，请使用web_helper插件查询\n或升级_query_rank_tw_lib为新版本')
+                    return
+                for _url in [_lib.tw_query_base % v for v in 
+                        (_guild_mode and _lib.tw_query_guild_revision or _lib.tw_query_self_revision)]:
+                    x = opener.open(urllib2.Request(_url, headers = _header)).read()
+                    show_it(x)
+                
+        else:#cn
+            from xml2dict import XML2Dict
+            po = plugin_vals['poster']
+            po.post('menu/menulist')
+            resp, ct = po.post('ranking/ranking', postdata='move=1&ranktype_id=0&top=0')
+            ct = XML2Dict().fromstring(ct).response.body.ranking
+            _user = ct.user_list.user
+            me = [_i for _i in _user if _i.id == plugin_vals['player'].id][0]
+            logger.info('%s:%s 收集品数量:%s\n'
+                        '可见区域内 Up:%s/%s Down:%s/%s' % (
+                            ct.ranktype_list.ranktype[-1].title, me.rank, me.battle_event_point,
+                            _user[0].rank, _user[0].battle_event_point,
+                            _user[-1].rank, _user[-1].battle_event_point)
+                        )
     return do
