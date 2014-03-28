3820e8b6e512f5b630400fc6dba210c42b3cc03c
fffonion <fffonion@gmail.com>
2014-03-29 01:02:01 +0800

    update plugin and floor evalstr

--- maclient.py
+++ maclient.py
@@ -42,8 +42,8 @@ GUILD_RACE_TYPE = ['11','12']
 eval_fairy_select = [('LIMIT', 'time_limit'), ('NOT_BATTLED', 'fairy.not_battled'), ('.lv', '.fairy.lv'), ('IS_MINE', 'user.id == self.player.id'), ('IS_WAKE_RARE', 'wake_rare'), ('IS_WAKE', 'wake'),  ('IS_GUILD', "fairy.race_type in GUILD_RACE_TYPE")]
 eval_fairy_select_carddeck = [('IS_MINE', 'discoverer_id == self.player.id'), ('IS_WAKE_RARE', 'wake_rare'), ('IS_WAKE', 'wake'), ('LIMIT', 'time_limit'),  ('IS_GUILD', "race_type in GUILD_RACE_TYPE"), ('NOT_BATTLED', 'not_battled'),('fairy.hp%', 'float(fairy.hp)/float(fairy.hp_max)')]
 eval_explore_area = [('IS_EVENT', "area_type=='1'"), ('IS_GUILD', "race_type in GUILD_RACE_TYPE"), ('IS_DAILY_EVENT', "id.startswith('5')"), ('NOT_FINNISHED', "prog_area!='100'")]
-eval_explore_floor = [('NOT_FINNISHED', 'progress!="100"'), ('not floor.IS_GUILD', "not area_race_type or area_race_type not in GUILD_RACE_TYPE"),
-('floor.IS_GUILD', "not area_race_type or area_race_type in GUILD_RACE_TYPE")]#little hack, 进入时area_race_type=0
+eval_explore_floor = [('NOT_FINNISHED', 'progress!="100"'), ('not floor.IS_GUILD', "(not area_race_type or area_race_type not in GUILD_RACE_TYPE)"),
+('floor.IS_GUILD', "(not area_race_type or area_race_type in GUILD_RACE_TYPE)")]#little hack, 进入时area_race_type=0
 eval_select_card = [('atk', 'power'), ('mid', 'master_card_id'), ('price', 'sale_price'), ('sid', 'serial_id'), ('holo', 'holography==1')]
 
 eval_task = []
--- plugins/fix_limit_time_tea.py
+++ plugins/fix_limit_time_tea.py
@@ -3,13 +3,14 @@ from _prototype import plugin_prototype
 # start meta
 __plugin_name__ = '台服优先嗑限时红绿茶'
 __author = 'fffonion'
-__version__ = 0.14
+__version__ = 0.15
 hooks = {'ENTER__use_item':1}
 extra_cmd = {}
 require_version = 1.68
 # end meta
 import time
 import maclient_smart
+MINOR_RED, MINOR_GREEN = 5010, 5009
 class plugin(plugin_prototype):
     def __init__(self):
         self.half_offset = maclient_smart.half_bc_offset_tw
@@ -22,20 +23,20 @@ class plugin(plugin_prototype):
             return
         self.already_inhook = True
         itemid = args[1]
-        minor_bc_cnt = mac.player.item.get_count(5005)
-        minor_ap_cnt = mac.player.item.get_count(5006)
+        minor_bc_cnt = mac.player.item.get_count(MINOR_RED)
+        minor_ap_cnt = mac.player.item.get_count(MINOR_GREEN)
         #10瓶秘药顶50%, 20瓶顶100%
         if int(itemid) == 2:
-            iid= 5005
+            iid= MINOR_RED
             cnt = min(20, minor_bc_cnt)
         elif int(itemid) == self.half_offset + 2:
-            iid= 5005
+            iid= MINOR_RED
             cnt = min(10, minor_bc_cnt)
         elif int(itemid) == 1:
-            iid= 5006
+            iid= MINOR_GREEN
             cnt = min(20, minor_ap_cnt)
         elif int(itemid) == self.half_offset + 1:
-            iid= 5006
+            iid= MINOR_GREEN
             cnt = min(10, minor_ap_cnt)
         if cnt == 0:
             self.already_inhook = False
