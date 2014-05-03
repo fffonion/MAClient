ce034edf566a6662b20933cb75aa6a385cb5d3c4
fffonion <fffonion@gmail.com>
2014-05-03 12:34:22 +0800

    Signed-off-by: fffonion <fffonion@gmail.com>

--- generate_meta.py
+++ generate_meta.py
@@ -3,12 +3,13 @@ import re
 import time
 import glob
 dirpath = 'D:\Dev\Python\Workspace\MAClient'
+blacklist = ['maclient_cli.py', 'maclient_network.py' ,'bgm.py']
 xml = '<?xml version="1.0" encoding="UTF-8"?><maclient><time>%d</time>' % int(time.time())
 p = '<plugin><name>%s</name><version>%s</version><dir>%s</dir></plugin>'
 s = '<script><name>%s</name><version>%s</version><dir>%s</dir></script>'
 for (pos, temp) in [('', s), ('plugins', p)]:
     for script in glob.glob(os.path.join(dirpath, pos,'*.py')):
-        if 'maclient_cli.py' in script or 'maclient_network.py' in script :
+        if sum([1 if bl in script else 0 for bl in blacklist]) > 0:
             continue
         _s = open(script).read()
         ver = re.findall('__version__[\s=\']*([^\'\s]+)[\']*', _s)
