970d3a1e03e33372ff372d29213d416a4026805a
fffonion <fffonion@gmail.com>
2014-06-13 20:48:37 +0800

    minor update

--- plugins/reward_box_card_helper.py
+++ plugins/reward_box_card_helper.py
@@ -4,7 +4,7 @@ from cross_platform import *
 # start meta
 __plugin_name__ = '领礼物盒里x星以上卡片'
 __author = 'fffonion'
-__version__ = 0.1
+__version__ = 0.2
 __tip__ = '如输入 rb !5 领取5星及以上卡片'
 hooks = {'ENTER_reward_box':1}
 extra_cmd = {}
@@ -19,16 +19,18 @@ class plugin(plugin_prototype):
             return
         if not rw_type.startswith('!'):
             return
-        star = rw_type[1:]
-        if not star.isdigit():
+        _star = rw_type[1:]
+        if not _star.rstrip('>').rstrip('<').isdigit():
             return
-        star = int(star)
+        star = int(_star.rstrip('>').rstrip('<'))
         mac = args[0]
         print(du8('将领取所有☆%d及以上卡片' % star))
         regexp = ''
         for c in mac.carddb:
             if mac.carddb[c][1] >= star:
-                regexp += mac.carddb[c][0].decode('utf-8') + '|'
+                # +商城快遞 是为了防止 炎夏型菲 匹配 炎夏型菲米娜
+                # 礼物标题被合并成类似 行樂型菲爾丹商城快遞行樂型菲爾丹
+                regexp += mac.carddb[c][0].decode('utf-8') + '商城快遞|' 
         regexp = regexp.rstrip('|')
         if not regexp:
             regexp = 'NOTHING-NOTHING-NOTHING'# make regexp not match anything
