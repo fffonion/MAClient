1e03da01c1873c492ec0761bfa8fd84a4d83c769
fffonion <fffonion@gmail.com>
2014-07-17 23:29:40 +0800

    reward_box_helper 认为BSR+ > ☆7

--- plugins/reward_box_card_helper.py
+++ plugins/reward_box_card_helper.py
@@ -4,7 +4,7 @@ from cross_platform import *
 # start meta
 __plugin_name__ = '领礼物盒里x星以上卡片'
 __author = 'fffonion'
-__version__ = 0.2
+__version__ = 0.3
 __tip__ = '如输入 rb !5 领取5星及以上卡片'
 hooks = {'ENTER_reward_box':1}
 extra_cmd = {}
@@ -20,18 +20,23 @@ class plugin(plugin_prototype):
         if not rw_type.startswith('!'):
             return
         _star = rw_type[1:]
-        if not _star.rstrip('>').rstrip('<').isdigit():
+        if not _star.rstrip('>').rstrip('<') in '123456789Bb':
             return
-        star = int(_star.rstrip('>').rstrip('<'))
+        if _star.rstrip('>').rstrip('<') in 'Bb':
+            star = 'B'
+            print(du8('将领取所有BSR+卡片'))
+        else:
+            star = int(_star.rstrip('>').rstrip('<'))
+            print(du8('将领取所有☆%d及以上卡片' % star))
         mac = args[0]
-        print(du8('将领取所有☆%d及以上卡片' % star))
         regexp = ''
         for c in mac.carddb:
-            if mac.carddb[c][1] >= star:
+            if (star in ['B', 6, 7] and 'BLACK' in mac.carddb[c][0]) or \
+                mac.carddb[c][1] >= star:
                 # +商城快遞 是为了防止 炎夏型菲 匹配 炎夏型菲米娜
                 # 礼物标题被合并成类似 行樂型菲爾丹商城快遞行樂型菲爾丹
                 regexp += mac.carddb[c][0].decode('utf-8') + '商城快遞|' 
-        regexp = regexp.rstrip('|')
+        regexp = regexp.rstrip('|').replace('(', '\(').replace(')', '\)').replace('?', '\?')
         if not regexp:
             regexp = 'NOTHING-NOTHING-NOTHING'# make regexp not match anything
         else:
